df4fc395bcfd1b736ec0c12d40a809fb11e12d68
user <user@Users-MBP-4.lan>
2023-04-05 14:01:29 +0700

    fixing swagger

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'API Base Project Swagger v1';
   }
 }
